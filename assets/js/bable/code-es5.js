1bb617cffcbb2c8c27dd5f6eee3bfd09863e40e5
devknowledge <ahmed.henteti.dev.knowledge@gmail.com>
2020-10-11 18:30:47 +0200

    improve code.js file

--- assets/js/bable/code-es5.js
+++ assets/js/bable/code-es5.js
@@ -1,43 +1,51 @@
 'use strict';
 
-document.querySelectorAll('.code-tabs + ul').forEach(function (codeTabs) {
-  try {
-    var tabsContentsId = 'tas-contents-id-' + Date.now() + Math.floor(Math.random() * 100);
-    var tabsLinks = '<div class="tab-links">';
-    var tabsContents = '<div>';
-    codeTabs.querySelectorAll('li').forEach(function (codeTab) {
-      var tabName = codeTab.querySelector('p').textContent;
-      var tabId = tabName + '-' + tabsContentsId;
-      var tabContent = codeTab.querySelector('div').innerHTML;
-      tabsLinks += '<button class="tab-link" onclick="openCode(event, \''
-        .concat(tabId, "', '")
-        .concat(tabsContentsId, '\')">')
-        .concat(tabName, '</button>');
-      tabsContents += '<div id="'.concat(tabId, '" class="tab-content">').concat(tabContent, '</div>');
-    });
-    tabsLinks += '</div>';
-    tabsContents + '</div>';
-    codeTabs.outerHTML = '<div id="'.concat(tabsContentsId, '">').concat(tabsLinks).concat(tabsContents, '</div>');
-    document.querySelector('#'.concat(tabsContentsId, ' .tab-link')).click();
-  } catch (error) {
-    console.error(error);
-  }
-});
-document.querySelectorAll('pre.highlight').forEach(function (code) {
-  try {
-    var codeLineNumbers = (code.innerHTML.match(/\n/g) || []).length;
-    var codeLineNumbersHtml = '<div class="code-line-number">';
+formatCodeTabs();
+addCodeLineNumbers();
 
-    for (var i = 1; i <= codeLineNumbers; i++) {
-      codeLineNumbersHtml += '<span>'.concat(i, '</span>');
+function formatCodeTabs() {
+  document.querySelectorAll('.code-tabs + ul').forEach(function (codeTabs) {
+    try {
+      var tabsContentsId = 'tas-contents-id-' + Date.now() + Math.floor(Math.random() * 100);
+      var tabsLinks = '<div class="tab-links">';
+      var tabsContents = '<div>';
+      codeTabs.querySelectorAll('li').forEach(function (codeTab) {
+        var tabName = codeTab.querySelector('p').textContent;
+        var tabId = tabName + '-' + tabsContentsId;
+        var tabContent = codeTab.querySelector('div').innerHTML;
+        tabsLinks += '<button class="tab-link" onclick="openCode(event, \''
+          .concat(tabId, "', '")
+          .concat(tabsContentsId, '\')">')
+          .concat(tabName, '</button>');
+        tabsContents += '<div id="'.concat(tabId, '" class="tab-content">').concat(tabContent, '</div>');
+      });
+      tabsLinks += '</div>';
+      tabsContents + '</div>';
+      codeTabs.outerHTML = '<div id="'.concat(tabsContentsId, '">').concat(tabsLinks).concat(tabsContents, '</div>');
+      document.querySelector('#'.concat(tabsContentsId, ' .tab-link')).click();
+    } catch (error) {
+      console.error(error);
     }
+  });
+}
 
-    codeLineNumbersHtml += '</div>';
-    code.insertAdjacentHTML('afterbegin', codeLineNumbersHtml);
-  } catch (error) {
-    console.error(error);
-  }
-});
+function addCodeLineNumbers() {
+  document.querySelectorAll('pre.highlight').forEach(function (code) {
+    try {
+      var codeLineNumbers = (code.innerHTML.match(/\n/g) || []).length;
+      var codeLineNumbersHtml = '<div class="code-line-number">';
+
+      for (var i = 1; i <= codeLineNumbers; i++) {
+        codeLineNumbersHtml += '<span>'.concat(i, '</span>');
+      }
+
+      codeLineNumbersHtml += '</div>';
+      code.insertAdjacentHTML('afterbegin', codeLineNumbersHtml);
+    } catch (error) {
+      console.error(error);
+    }
+  });
+}
 
 function openCode(event, tabId, tabsContentsId) {
   var tabsContents = document.getElementById(tabsContentsId);
--- assets/js/code.js
+++ assets/js/code.js
@@ -1,37 +1,44 @@
-document.querySelectorAll('.code-tabs + ul').forEach((codeTabs) => {
-  try {
-    const tabsContentsId = 'tas-contents-id-' + Date.now() + Math.floor(Math.random() * 100);
-    let tabsLinks = '<div class="tab-links">';
-    let tabsContents = `<div>`;
-    codeTabs.querySelectorAll('li').forEach((codeTab) => {
-      const tabName = codeTab.querySelector('p').textContent;
-      const tabId = tabName + '-' + tabsContentsId;
-      const tabContent = codeTab.querySelector('div').innerHTML;
-      tabsLinks += `<button class="tab-link" onclick="openCode(event, '${tabId}', '${tabsContentsId}')">${tabName}</button>`;
-      tabsContents += `<div id="${tabId}" class="tab-content">${tabContent}</div>`;
-    });
-    tabsLinks += '</div>';
-    tabsContents + '</div>';
-    codeTabs.outerHTML = `<div id="${tabsContentsId}">${tabsLinks}${tabsContents}</div>`;
-    document.querySelector(`#${tabsContentsId} .tab-link`).click();
-  } catch (error) {
-    console.error(error);
-  }
-});
+formatCodeTabs();
+addCodeLineNumbers();
 
-document.querySelectorAll('pre.highlight').forEach((code) => {
-  try {
-    const codeLineNumbers = (code.innerHTML.match(/\n/g) || []).length;
-    let codeLineNumbersHtml = '<div class="code-line-number">';
-    for (let i = 1; i <= codeLineNumbers; i++) {
-      codeLineNumbersHtml += `<span>${i}</span>`;
+function formatCodeTabs() {
+  document.querySelectorAll('.code-tabs + ul').forEach((codeTabs) => {
+    try {
+      const tabsContentsId = 'tas-contents-id-' + Date.now() + Math.floor(Math.random() * 100);
+      let tabsLinks = '<div class="tab-links">';
+      let tabsContents = `<div>`;
+      codeTabs.querySelectorAll('li').forEach((codeTab) => {
+        const tabName = codeTab.querySelector('p').textContent;
+        const tabId = tabName + '-' + tabsContentsId;
+        const tabContent = codeTab.querySelector('div').innerHTML;
+        tabsLinks += `<button class="tab-link" onclick="openCode(event, '${tabId}', '${tabsContentsId}')">${tabName}</button>`;
+        tabsContents += `<div id="${tabId}" class="tab-content">${tabContent}</div>`;
+      });
+      tabsLinks += '</div>';
+      tabsContents + '</div>';
+      codeTabs.outerHTML = `<div id="${tabsContentsId}">${tabsLinks}${tabsContents}</div>`;
+      document.querySelector(`#${tabsContentsId} .tab-link`).click();
+    } catch (error) {
+      console.error(error);
     }
-    codeLineNumbersHtml += '</div>';
-    code.insertAdjacentHTML('afterbegin', codeLineNumbersHtml);
-  } catch (error) {
-    console.error(error);
-  }
-});
+  });
+}
+
+function addCodeLineNumbers() {
+  document.querySelectorAll('pre.highlight').forEach((code) => {
+    try {
+      const codeLineNumbers = (code.innerHTML.match(/\n/g) || []).length;
+      let codeLineNumbersHtml = '<div class="code-line-number">';
+      for (let i = 1; i <= codeLineNumbers; i++) {
+        codeLineNumbersHtml += `<span>${i}</span>`;
+      }
+      codeLineNumbersHtml += '</div>';
+      code.insertAdjacentHTML('afterbegin', codeLineNumbersHtml);
+    } catch (error) {
+      console.error(error);
+    }
+  });
+}
 
 function openCode(event, tabId, tabsContentsId) {
   const tabsContents = document.getElementById(tabsContentsId);
