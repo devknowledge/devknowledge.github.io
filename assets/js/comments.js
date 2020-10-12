f3a970cc8d791292253734bc75b178ceeb22f070
devknowledge <ahmed.henteti.dev.knowledge@gmail.com>
2020-10-12 23:39:07 +0200

    add post comments

--- assets/js/comments.js
+++ assets/js/comments.js
@@ -1,4 +1,5 @@
-const comments = document.getElementById('comments');
+const joinGithubDiscussionElement = document.getElementById('JoinGithubDiscussion');
+const commentsElement = document.getElementById('Comments');
 fetchCurrentPostAssociatedGithubIssue();
 
 function fetchCurrentPostAssociatedGithubIssue() {
@@ -28,23 +29,68 @@ function handleGithubSearchIssuesResponse(responseText) {
 
 function renderJoinPostDiscussionOnGithub(url) {
   const html = /* html */ `
-    <p class="join-discussion">Join the discussion fir this article on <a href="${url}" target="_blank">this github ticket</a></p>  
+    <p class="comments-join-discussion">Join the discussion for this article on <a href="${url}" target="_blank">this github ticket</a></p>  
   `;
-  comments.insertAdjacentHTML('afterend', html);
+  joinGithubDiscussionElement.outerHTML = html;
 }
 
 function fetchCurrentPostComments(url) {
-  fetch(url)
-    .then((response) => response.text())
-    .then(handleGithubGetCommentsResponse)
-    .catch((error) => console.log('error', error));
+  try {
+    fetch(url)
+      .then((response) => response.text())
+      .then((response) => renderPostsComments(JSON.parse(response)))
+      .catch((error) => console.log('error', error));
+  } catch (error) {
+    console.log('error in fetchCurrentPostComments method: ' + error);
+  }
 }
 
-function handleGithubGetCommentsResponse(responseText) {
+function renderPostsComments(comments) {
   try {
-    const response = JSON.parse(responseText);
-    console.log(response);
+    let html = '';
+    comments.forEach((comment) => {
+      const commentCreatedSince = since(comment.created_at);
+      html += /* html */ `
+        <div class="comment">
+          <img class="comment__user-avatar" src="${comment.user.avatar_url}" alt="avatar">
+          <div class="comment__container">
+            <div class="comment__header">
+              <p class="comment_user-name">${comment.user.login}</p>
+              <p class="comment_created-since">${commentCreatedSince}</p>
+            </div>
+            <p class="comment__body">${comment.body}</p>
+          </div>
+        </div>
+      `;
+    });
+    commentsElement.outerHTML = html;
   } catch (error) {
-    console.log(error);
+    console.log('error in renderPostsComments method: ' + error);
   }
+}
+
+function since(date) {
+  const seconds = Math.floor((new Date() - new Date(date)) / 1000);
+  let interval = seconds / 31536000;
+
+  if (interval > 1) {
+    return Math.floor(interval) + ' years ago';
+  }
+  interval = seconds / 2592000;
+  if (interval > 1) {
+    return Math.floor(interval) + ' months ago';
+  }
+  interval = seconds / 86400;
+  if (interval > 1) {
+    return Math.floor(interval) + ' days ago';
+  }
+  interval = seconds / 3600;
+  if (interval > 1) {
+    return Math.floor(interval) + ' hours ago';
+  }
+  interval = seconds / 60;
+  if (interval > 1) {
+    return Math.floor(interval) + ' minutes ago';
+  }
+  return Math.floor(seconds) + ' seconds ago';
 }
