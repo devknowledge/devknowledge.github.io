7320e09622ef6600965d396db96a35034017a4a0
Ahmed HENTETI <ahmad.henteti@gmail.com>
2020-10-20 23:22:46 +0200

    compile scss with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,17 @@
 const path = require('path');
+const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  mode: 'production',
   entry: path.join(__dirname, 'webpack', 'main'),
   output: {
     filename: '[name]-bundle.js',
-    path: path.resolve(__dirname, 'assets/js'),
+    path: path.resolve(__dirname, 'assets'),
   },
+  plugins: [
+    new ExtractTextWebpackPlugin({
+      filename: '[name]-bundle.css',
+    }),
+  ],
   module: {
     rules: [
       {
@@ -14,9 +19,16 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader',
       },
+      {
+        test: /\.s?[ac]ss$/,
+        use: ExtractTextWebpackPlugin.extract({
+          use: ['css-loader', 'postcss-loader', 'sass-loader'],
+        }),
+      },
     ],
   },
   resolve: {
+    modules: [path.resolve('./webpack'), path.resolve('./node_modules')],
     extensions: ['.json', '.js'],
   },
 };
